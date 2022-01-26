99707b05c769b161e0e3045fbeaa728d3f81218c
Maxim <markushitstep@gmail.com>
2022-01-26 17:09:48 +0200

    code refactoring using redux lib

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './styles.css'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { rootReducer } from './redux/rootReducer';
+import { increment, decrement, asyncIncrement } from './redux/rootActions';
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+let store = createStore(rootReducer, 0,applyMiddleware(thunk) );
+
+
+addBtn.addEventListener('click', () => {
+    store.dispatch( increment() );
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement());
+})
+
+asyncBtn.addEventListener('click', () => {
+    store.dispatch(asyncIncrement());
+})
+
+themeBtn.addEventListener('click', () => {
+    //document.body.classList.toggle('dark');
+
+})
+
+store.subscribe(() => {
+    const state = store.getState();
+
+    counter.textContent = state;
+});
+
+store.dispatch({type: 'INIT_APPLICATION'});
+
+//render();
+
+
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,11 @@
+import { DECREMENT, INCREMENT } from "./rootTypes"
+
+export function rootReducer(state, action) {
+    if(action.type === INCREMENT ) {
+        return state + 1
+    } else if (action.type === DECREMENT ) {
+        return state - 1
+    }
+    
+    return state
+} 
